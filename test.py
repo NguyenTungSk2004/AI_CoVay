1ced7057504a2e8cbf3be854f8b0ea43495ca086
NguyenTungSk2004 <nguyentungsk2004@gmail.com>
2024-10-04 22:52:44 +0700

    update ai

---
+++ test.py
@@ -0,0 +1,30 @@
+from rules import Rules
+class KO_rule:
+    def __init__(self):
+        self.current_board = None  
+        self.previous_board = None  
+
+    def check_duplicate(self, board_real):
+        board = [row[:] for row in board_real]
+        
+        if self.current_board is None:
+            self.current_board = board
+            print("Nuoc dau tien")
+            return True
+        
+        if board != self.previous_board: 
+            self.previous_board = self.current_board
+            self.current_board = board
+
+        if board == self.previous_board:
+            print("Nuoc di khong hop le")
+            return False
+
+        print("Nuoc di hop le")
+        return True
+    def is_repeated_state(self,board,x,y,typeChess):
+        #tạo cái board với nước đi là (x,y) ở đây
+        board = [row[:] for row in board]
+        board[x][y] = typeChess
+        Rules.capture_stones(board,-typeChess)
+        return self.check_duplicate(board)
